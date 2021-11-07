f3df52c0adcb4979b81723ae457366dcd477503b
vacexpfleger <c4n4l123@gmail.com>
2021-11-07 18:38:08 +0100

    added file loading

--- projekt-python-master/03-types/structures.py
+++ projekt-python-master/03-types/structures.py
@@ -1,3 +1,16 @@
+def txtRead(path):
+    try:
+        with open(path, mode='r', encoding='utf-8') as file:
+            return file.read()
+    except FileNotFoundError as error:
+        return "File not found"
+    except Exception as error:
+        return "Loading failed"
+    finally:
+        file.close()
+    return text
+
+
 def charFrequency(string):
     frequency = list({n: string.count(n) for n in set(string)}.items())
     frequency.sort(reverse=True, key=lambda y: y[1])
@@ -6,5 +19,4 @@ def charFrequency(string):
         print(i)
 
 
-sentence = "Tři sta třicet tři stříbrných stříkaček přestříkalo přes tři sta třicet tři stříbrných střech."
-charFrequency(sentence)
+charFrequency(txtRead('file.txt'))
